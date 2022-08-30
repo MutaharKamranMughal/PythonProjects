17599c1161bcb9e9c25f40b94c3521bb08704502
MutaharKamranMughal <108257950+MutaharKamranMughal@users.noreply.github.com>
2022-08-30 15:28:07 +0500

    Add files via upload

---
+++ WordReplacement/app.py
@@ -0,0 +1,9 @@
+def replace_word():
+
+    str = "Hi guys I am Tommy and hi hi hi hi."
+    str
+    word_to_replace = input("Enter the Word to Replace: ")
+    word_replacement = input("Enter the word replacement: ")
+    print(str.replace(word_to_replace, word_replacement))
+
+replace_word()
